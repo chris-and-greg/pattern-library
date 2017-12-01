34249ce6078905347c183cb518168a38e389ddda
DeathMagic Media <deathmagicmedia@gmail.com>
2017-12-01 21:03:36 +0200

    Initial commit

---
+++ asset/js/script.js
@@ -0,0 +1,9 @@
+/*
+ * Framework Name (URL)
+ * Copyright (c) 2009-2017 DeathMagic Media
+ * Copyright (c) 2009-2017 Gregori Marzio
+ * Copyright (c) 2016-2017 Christine Nyman
+ * Licensed under MIT (URL/license.txt)
+ */
+
+
